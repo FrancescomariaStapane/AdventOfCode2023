4309546a0ebf30a5a9e9ffac912312580163c7d8
Francescomaria Stapane <francescomariastapane@gmail.com>
2023-12-05 19:33:05 +0100

    giorno 5 parte 1

--- D5/Sol1.py
+++ D5/Sol1.py
@@ -1,36 +1,49 @@
 file = open("input.txt", "r")
 lines = file.readlines()
+seeds = [int(i) for i in lines[0][7:-1].split(" ")]
+locations=[]
+# seed_to_soil = []
+# soil_to_fertilizer = []
+# fertilizer_to_water = []
+# water_to_light = []
+# light_to_temperature = []
+# temperature_to_humidity = []
+# humidity_to_location = []
 
-seeds = lines[0][7:-1].split(" ")
-seed_to_soil=[]
-soil_to_fertilizer = []
-fertilizer_to_water = []
-water_to_light = []
-light_to_temperature = []
-temperature_to_humidity = []
-humidity_to_location = []
+maps = [[], [], [], [], [], [], []]
+
+
+# maps = [seed_to_soil, soil_to_fertilizer, fertilizer_to_water, water_to_light, light_to_temperature,temperature_to_humidity, humidity_to_location]
 
 
 def parseMap(lines_, line_index, map):
-    while len((lines[line_index])) > 1:
+    while len((lines_[line_index])) > 1:
         map.append(lines[line_index][:-1].split(" "))
         line_index += 1
     line_index += 2
     return line_index
 
 
+def findNext(start, map_):
+    for row in map_:
+        destination_start = row[0]
+        source_start = row[1]
+        range_ = row[2]
+        if source_start <= start <= source_start + range_:
+            return destination_start + start - source_start
+    return start
+
+
 line_index = 3
-line_index = parseMap(lines, line_index, seed_to_soil)
-line_index = parseMap(lines, line_index, soil_to_fertilizer)
-line_index = parseMap(lines, line_index, fertilizer_to_water)
-line_index = parseMap(lines, line_index, water_to_light)
-line_index = parseMap(lines, line_index, light_to_temperature)
-line_index = parseMap(lines, line_index, temperature_to_humidity)
-line_index = parseMap(lines, line_index, humidity_to_location)
-print(seed_to_soil)
-print(soil_to_fertilizer)
-print(fertilizer_to_water)
-print(water_to_light)
-print(light_to_temperature)
-print(temperature_to_humidity)
-print(humidity_to_location)
+for map_ in maps:
+    line_index = parseMap(lines, line_index, map_)
+
+for map_ in maps:
+    for i in range(len(map_)):
+        map_[i]=[int(j) for j in map_[i]]
+for seed in seeds:
+    start=seed
+    for map_ in maps:
+        start=findNext(start,map_)
+    locations.append(start)
+print(min(locations))
