05a21a8f7d211207519689a9d706c85fab9ad221
Francescomaria Stapane <francescomariastapane@gmail.com>
2023-12-05 23:52:02 +0100

    giorno 5 parte 2 (kinda)

--- D5/Sol1.py
+++ D5/Sol1.py
@@ -1,21 +1,12 @@
 file = open("input.txt", "r")
 lines = file.readlines()
 seeds = [int(i) for i in lines[0][7:-1].split(" ")]
-locations=[]
-# seed_to_soil = []
-# soil_to_fertilizer = []
-# fertilizer_to_water = []
-# water_to_light = []
-# light_to_temperature = []
-# temperature_to_humidity = []
-# humidity_to_location = []
+locations = []
 
 maps = [[], [], [], [], [], [], []]
 
 
-# maps = [seed_to_soil, soil_to_fertilizer, fertilizer_to_water, water_to_light, light_to_temperature,temperature_to_humidity, humidity_to_location]
-
-
+# parses a single map and returns index of the next useful line
 def parseMap(lines_, line_index, map):
     while len((lines_[line_index])) > 1:
         map.append(lines[line_index][:-1].split(" "))
@@ -24,6 +15,7 @@ def parseMap(lines_, line_index, map):
     return line_index
 
 
+# returns the destination of a map given the start and the map
 def findNext(start, map_):
     for row in map_:
         destination_start = row[0]
@@ -34,16 +26,20 @@ def findNext(start, map_):
     return start
 
 
+# parsing
 line_index = 3
 for map_ in maps:
     line_index = parseMap(lines, line_index, map_)
 
+# casting strings to ints
 for map_ in maps:
     for i in range(len(map_)):
-        map_[i]=[int(j) for j in map_[i]]
+        map_[i] = [int(j) for j in map_[i]]
+
+# solve
 for seed in seeds:
-    start=seed
+    start = seed
     for map_ in maps:
-        start=findNext(start,map_)
+        start = findNext(start, map_)
     locations.append(start)
 print(min(locations))
